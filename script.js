be7b586832d697c22b580db99097d1b9eb9a0bb8
Alex Maurer <alex.maurer171@gmail.com>
2021-03-17 11:52:58 +0100

    Created the pages, the html skeleton and the navigation

---
+++ script.js
@@ -0,0 +1,6 @@
+/*const button = document.getElementById("button_nav");
+const navigation = document.querySelector(".link_nav");
+
+button.addEventListener("click", () => {
+    navigation.style.display = 'block';
+});*/
