891a8de9efa8dcdfec4d2c23c1352b896f81ac69
Amanel <amanel.gauriat@gmail.com>
2021-03-18 09:59:21 +0100

    swipe Attempt

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navigation = document.querySelector(".link_nav");
 button.addEventListener("click", () => {
     navigation.style.display = 'block';
 });*/
+
+
+const _C = document.querySelector('.container'),
+    N = _C.children.length;
+
+_C.style.setProperty('--n', N);
